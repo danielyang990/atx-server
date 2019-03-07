eb8c0c8f76982b544ef52a64e98a950476ba1b6b
daniel <danielyang990@gmail.com>
2019-03-07 09:43:55 +0800

    feat: change makefile

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 dc_cmd=docker-compose -p atx
-atx_server_addr=192.168.147.230:8000
+atx_server_addr=192.168.147.235:8000
 # --serial $SERIAL
 
 up:
 	$(dc_cmd) up -d --build
+
+push:
+	docker push firewolf990/atxserver
 
 down:
 	$(dc_cmd) down
@@ -18,4 +21,5 @@ log:
 	$(dc_cmd) logs -f atxserver
 
 init:
-	python -m uiautomator2 init --server $(atx_server_addr)
+	./u2init -s $(atx_server_addr) --agent=0.5.4
+	# python -m uiautomator2 init --server $(atx_server_addr)
---
+++ init-resources.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -
+#
+
+set -e
+
+# Verisons modify here
+ATX_AGENT_VERSION=0.5.4
+UIAUTOMATOR_APK_VERSION=1.1.5
+STF_BINARIES_VERSION=0.2
+
+RESDIR="resources"
+# Download resources
+mkdir -p $RESDIR
+
+GITHUB_URL="https://github.com"
+# GITHUB_URL="https://github-mirror.open.netease.com"
+
+# https://github.com/openatx/stf-binaries/archive/0.1.zip
+
+download_apk(){
+	APK_BASE_URL="$GITHUB_URL/openatx/android-uiautomator-server/releases/download/${UIAUTOMATOR_APK_VERSION}"
+	wget -O $RESDIR/app-uiautomator.apk "$APK_BASE_URL/app-uiautomator.apk" # app-1.0.4/app-uiautomator.apk
+	wget -O $RESDIR/app-uiautomator-test.apk "$APK_BASE_URL/app-uiautomator-test.apk"
+}
+
+download_stf(){
+	## minicap+minitouch
+	wget -O $RESDIR/stf-binaries.zip "$GITHUB_URL/openatx/stf-binaries/archive/$STF_BINARIES_VERSION.zip"
+	unzip -o -d $RESDIR/ $RESDIR/stf-binaries.zip
+}
+
+download_atx(){
+	## atx-agent
+	wget -O $RESDIR/atx-agent-$ATX_AGENT_VERSION.tar.gz "$GITHUB_URL/openatx/atx-agent/releases/download/$ATX_AGENT_VERSION/atx-agent_${ATX_AGENT_VERSION}_linux_armv6.tar.gz"
+	tar -C $RESDIR/ -xzvf $RESDIR/atx-agent-$ATX_AGENT_VERSION.tar.gz atx-agent
+}
+
+download_atx
+download_apk
+download_stf
+
+echo "Everything is downloaded. ^_^"
